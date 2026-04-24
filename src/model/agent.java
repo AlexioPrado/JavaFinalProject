c7b4b2cb46de4de57eff60e387a366d9bb256738
Marcus Prado <mgprado1@my.waketech.edu>
2026-04-24 14:19:20 +0000

    continue

---
+++ src/model/agent.java
@@ -0,0 +1,19 @@
+/**
+ * Marcus Alexio Prado 
+ * Course: Adv Java 1
+ * Date: 4/24/26
+ * Last Modified: 4/25/26
+ * 
+ */
+
+public class agent {
+    public String name;
+    public Int maxHealth;
+    public Int health;
+    public Int maxEnergy;
+    public Int energy;
+
+
+    // CREATE A BUFF METHOD TO RETURN AN ARRAYLIST OF BUFFS. an array list of strings. 
+    // However you do it, maybe buffs are an object and you get its string name or whatever
+}
--- src/view/gameView.java
+++ src/view/gameView.java
@@ -1,6 +1,48 @@
+import java.util.ArrayList;
+
 /**
  * Marcus Alexio Prado
  * Course: Adv Java
  * Date: 4/23/26
  * 
  */
+
+public class gameView{
+
+    public void showChoices(String message, ArrayList<E> options){
+        System.out.println(message);
+        for (i = 1; i < options.size(); i++){
+            System.out.println(i + ". " + options.get(i));
+        }
+    }
+
+    public void showMessage(String message){
+        System.out.println(message);
+    }
+
+    public void showCharacterStats(agent character){
+        System.out.println("+--------------------------------------+");
+        System.out.println("| %-18s %18s |%n", "Name: " + character.name, "HP: " + character.health + "/" + character.maxHealth);
+        System.out.println("|--------------------------------------|");
+        System.out.println("| Buffs:                               |");
+
+        for (String buffs : character.getBuffs()){
+            System.out.println("|  - %-33s |%n", buffs);
+        }
+
+        System.out.println("+--------------------------------------+");
+    }
+
+    public void showCharacterKit(agent Character){
+        System.out.println("+--------------------------------------+");
+        System.out.println("| %-36s |%n", "Name: " + character.name);
+        System.out.println("|--------------------------------------|");
+        System.out.println("| Normal Attack:                       |");
+        System.out.println()
+        System.out.println("| Skill:                               |");
+
+        System.out.println("| Ultimate:                            |");
+
+        System.out.println("+--------------------------------------+");
+    }
+}
