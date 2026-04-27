e73965182314f0c15e5ade7a3f1bb1c5c1690747
Marcus Prado <mgprado1@my.waketech.edu>
2026-04-27 13:12:48 +0000

    continuing agent class

--- src/model/agent.java
+++ src/model/agent.java
@@ -6,7 +6,7 @@
  * 
  */
 
-public class agent {
+public abstract class agent {
     protected String name;
     protected Int maxHealth;
     protected Int health;
@@ -19,6 +19,45 @@ public class agent {
     protected Int skDmg;
     protected Int ultDmg;
 
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    public void gainEnergy(){
+        energy += 1;
+        if (energy > maxEnergy){
+            energy = maxEnergy;
+        }
+    }
+
+    public void useEnergy(){
+        energy = 0;
+    }
+
+    public void takeDamage(Int dmg){
+        health -= dmg;
+    }
+
+    public string getNormalAttackName(){
+        return naName;
+    }
+
+    public string getSkillkName(){
+        return skName;
+    }
+
+    public string getUltName(){
+        return ultName;
+    }
+
+    public abstract string getNormalAttackInfo();
+    public abstract string getSkillAttackInfo();
+    public abstract string getUltimateAttackInfo();
+
+    public abstract void normalAttack(Enemy enemy);
+    public abstract void skillAttack(Enemy enemy);
+    public abstract void ultimateAttack(Enemy enemy);
+    
 
     //CREATE DMG STUN INCREASE METHOD
     //DIFFERENT FOR EVERYONE
