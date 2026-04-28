83484f71ad7983ce4335629f86c8bf6cc3e4ee17
Marcus Prado <mgprado1@my.waketech.edu>
2026-04-28 04:24:14 +0000

    continue working

---
+++ src/model/enemySubClass/agentChiikawa.java
@@ -0,0 +1,18 @@
+/**
+ * Marcus Alexio Prado
+ * Course: Adv Java
+ * Date: 4/28/26
+ * Last Modified: 4/28/26
+ * 
+ */
+
+public class agentChiikawa extends enemy {
+    public agentChiikawa(){
+        this.name = "Chiikawa";
+        this.health = 18;
+        this.maxHealth = 18;
+        this.attack = 4;
+        this.isStun = false;
+        this.stunDuration = 0;
+    }
+}
